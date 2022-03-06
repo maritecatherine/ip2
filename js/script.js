75f0569480ae4301a7f8430670ec5db9e341280e
Catherine <maritecatherine91@gmail.com>
2022-03-06 07:37:12 +0300

    add variable for akan male names

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var femaleNames =["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
