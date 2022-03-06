1c8e6a98993e4b3e3ed63aebfa8ab3205f846627
Catherine <maritecatherine91@gmail.com>
2022-03-06 08:16:43 +0300

    Add a validation rule for the month value

--- js/script.js
+++ js/script.js
@@ -6,7 +6,14 @@ function validate() {
     if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value <= 1900) {
         alert("Please provide a valid year");
         document.myForm.year.focus();
-        return false
+        return false;
 
     }
+    else if( document.myForm.month.value == "" || document.myForm.month.value.length !== 2 || isNaN(document.myForm.month.value) ||
+    document.myForm.month.value > 31 || document.myForm.month.value <= 0){
+        alert("Please provide a valid month");
+        document.myForm.month.focus();
+        return false;
+    }
+    else if (document.myForm.day)
 }
