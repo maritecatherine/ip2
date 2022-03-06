bb0270643d9d6d53f3a150f816dbe1ace321ddc7
Catherine <maritecatherine91@gmail.com>
2022-03-06 08:29:59 +0300

    Add day value validation rule

--- js/script.js
+++ js/script.js
@@ -4,16 +4,21 @@ var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 function validate() {
     if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value <= 1900) {
-        alert("Please provide a valid year");
+        alert("Please provide a valid year value!");
         document.myForm.year.focus();
         return false;
 
     }
     else if( document.myForm.month.value == "" || document.myForm.month.value.length !== 2 || isNaN(document.myForm.month.value) ||
-    document.myForm.month.value > 31 || document.myForm.month.value <= 0){
-        alert("Please provide a valid month");
+    document.myForm.month.value > 12 || document.myForm.month.value <= 0){
+        alert("Please provide a valid month value!");
         document.myForm.month.focus();
         return false;
     }
-    else if (document.myForm.day)
+    else if (document.myForm.day.value == "" || document.myForm.day.value.length != 2 || isNaN(document.myForm.day.value) ||
+    document.myForm.day.value > 31 || document.myForm.day.value <= 0) {
+        alert("Please provide a valid day value!");
+        document.myForm.day.focus();
+        return false;
+    }
 }
