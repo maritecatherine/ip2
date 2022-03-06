b7892152e00790d056761b61bc0bb93c19eaf512
Catherine <maritecatherine91@gmail.com>
2022-03-06 07:56:06 +0300

    Add a git condition for value year

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua", "Yaa", "Afua", "Ama"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+function validate() {
+    if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value <= 1900) {
+        alert("Please provide a valid year");
+        document.myForm.year.focus();
+        return false
+
+    }
+}
