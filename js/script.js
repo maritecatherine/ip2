d9c4b024f962f9bc93995506546f956a8604e6d4
Catherine <maritecatherine91@gmail.com>
2022-03-07 10:59:43 +0300

    add onclick functio and edit some errors

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturda
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua", "Yaa", "Afua", "Ama"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
+var dayValue, d, CC, DD, YY, MM;
 
 function validate() {
 var isGender = document.getElementsByName("gender");
-    if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value <= 1900) {
+    if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value > 2100 || document.myForm.year.value <= 1900) {
         alert("Please provide a valid year value!");
         document.myForm.year.focus();
         return false;
 
     }
-    else if( document.myForm.month.value == "" || document.myForm.month.value.length !== 2 || isNaN(document.myForm.month.value) ||
+    else if( document.myForm.month.value == "" || document.myForm.month.value.length != 2 || isNaN(document.myForm.month.value) ||
     document.myForm.month.value > 12 || document.myForm.month.value <= 0){
         alert("Please provide a valid month value!");
         document.myForm.month.focus();
@@ -32,18 +33,19 @@ var isGender = document.getElementsByName("gender");
     }
     
 }
-calDayValue => {
+function calDayValue() {
     year = document.getElementById("year").value;
     CC = parseInt(year.substring(0,2));
     DD = parseInt(year.substring(2,4));
     MM = parseInt(document.getElementById("month").value);
     DD = parseInt(document.getElementById("day").value);
 
-    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    console.log(Math.floor(d));
+    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+    console.log(d);
+    return (math.floor(d))
     
 }
-getGender => {
+function getGender() {
     var isGender = document.getElementsByName("gender");
     if(isGender[0].checked == true){
         var gender = "male";
@@ -56,7 +58,7 @@ getGender => {
     }
 
     switch(gender) {
-    case "male":
+        case "male":
         if(dayValue == 1) {
             alert("Your Akan name is " +maleNames[0]+"!");
         }
@@ -102,11 +104,14 @@ getGender => {
                 alert("Your Akan name is " +femaleNames[6]+"!");
             }
             break
+
             default:
+       
+
+    }
 
 }
 
-}
 function yourName(){
     dayValue = calDayValue();
     getGender();
